1dab831c6d8dd61bf9f517565eabeb544a97a0ec
tufayel2007 <ta07092007@gmail.com>
2025-12-03 20:19:25 +0600

    first commit

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -2,19 +2,19 @@ export const educations = [
   {
     id: 1,
     title: "Bachelor Degree",
-    duration: "2017 - 2021",
-    institution: "Dr. A. P. J. Abdul Kalam Technical University, Lucknow, ",
+    duration: "2024 - 2025",
+    institution: "Sylhet Polytechnic Institute.  ",
   },
   {
     id: 2,
     title: "Higher Secondary Certificate",
-    duration: "2014 - 2016",
-    institution: "Subhash Academy (CBSE)",
+    duration: "2022 - 2024",
+    institution: "Netrakona Govt.Technical School And College ,{ET}.",
   },
   {
     id: 3,
     title: "Secondary School Certificate",
-    duration: "2012 - 2014",
-    institution: "Subhash Academy (CBSE)",
+    duration: "2020 - 2022",
+    institution: "Netrakona Govt.Technical School And College, {ET}.",
   },
 ];
